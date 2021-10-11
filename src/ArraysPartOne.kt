ecfbff96850b674b4cb875080f05cd590aef135a
mschultz366 <mschultz366@students.mchenry.edu>
2021-10-11 05:15:44 -0500

    Initial commit

---
+++ src/ArraysPartOne.kt
@@ -0,0 +1,8 @@
+//Program contains supply list for pretzel stand
+fun main(args: Array<String>) {
+    var pretzelCartSupplies = arrayOf<String>("Dough","Trays","Cheese", "Salt","Napkins")
+
+    for (supply in pretzelCartSupplies) {
+        print("$supply\n")
+    }
+}
